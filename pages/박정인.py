20edfb22df4e7ba7ee931972a2743d074ab0bebe
2553s2610715-cmyk <2553s2610715@g.cnees.kr>
2026-06-16 10:05:22 +0900

    Update 박정인.py

--- pages/박정인.py
+++ pages/박정인.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 st.title("진로 알아보기")
 
-st.write("박정인")
-st.write("양민준")
-st.write("이예솔")
+career = st.text_input("당신의 진로는 무엇인가요?")
 
-st.text_input("당신의 진로는 무엇인가요?")
+if career:
+    st.write("입력한 진로:", career)
