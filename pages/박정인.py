3f3f7c2f95f445cb15466ae6b491e8a29995879f
2553s2610715-cmyk <2553s2610715@g.cnees.kr>
2026-06-16 10:04:17 +0900

    Update 박정인.py

--- pages/박정인.py
+++ pages/박정인.py
@@ -1,18 +1,9 @@
 import streamlit as st
 
-st.title("🎯 진로 추천기")
+st.title("진로 알아보기")
 
-job = st.selectbox(
-    "관심 분야를 선택하세요",
-    ["프로그래밍", "디자인", "마케팅", "과학"]
-)
+st.write("박정인")
+st.write("양민준")
+st.write("이예솔")
 
-if st.button("추천받기"):
-    if job == "프로그래밍":
-        st.success("추천 직업: 개발자, AI 엔지니어")
-    elif job == "디자인":
-        st.success("추천 직업: UI/UX 디자이너")
-    elif job == "마케팅":
-        st.success("추천 직업: 마케터")
-    else:
-        st.success("추천 직업: 연구원, 과학자")
+st.text_input("당신의 진로는 무엇인가요?")
